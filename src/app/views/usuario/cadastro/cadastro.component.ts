a645ac62bc4f8d6157756ec6f032d5307477be77
Papa Neto <papaneto@hotmail.com>
2018-04-21 22:40:44 -0400

    crud usuario inicial OK

--- src/app/views/usuario/cadastro/cadastro.component.ts
+++ src/app/views/usuario/cadastro/cadastro.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../../model/usuario/usuario.model';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { UsuarioService } from '../../../service/usuario/usuario.service';
+import { FeedbackService } from '../../../service/feedback/feedback.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +13,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  private usuario: Usuario;
+
+    private edit: boolean;
+    private txtBtnSubmit = '';
+    private txtHeader = '';
+
+  constructor(
+    private service: UsuarioService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private feedback: FeedbackService
+  ) { }
 
   ngOnInit() {
+    this.usuario = new Usuario();
+
+    this.welcome();
+  }
+
+  welcome() {
+    this.route.params.subscribe(
+      (params: any) => {
+          if (params['id'] != null) {
+              this.edit = true;
+              this.txtBtnSubmit = 'editar';
+              this.txtHeader = 'Edição';
+              this.getusuario(params['id']);
+            } else {
+              this.edit = false;
+              this.txtBtnSubmit = 'cadastrar';
+              this.txtHeader = 'Cadastro';
+          }
+      }
+    );
+  }
+
+  getusuario(id: number): void {
+    this.service.getOne(id).subscribe(
+      success => {
+        this.usuario = success;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  save(usuario: Usuario): void {
+
+    if (this.edit) {
+      this.service.update(usuario).subscribe(
+        success => {
+          this.feedback.openSnackBar(success.msg);
+          this.router.navigate(['/usuario']);
+        },
+        error => {
+          this.feedback.openSnackBar(error);
+        }
+      );
+    } else {
+      this.service.save(usuario).subscribe(
+        success => {
+          this.feedback.openSnackBar(success.msg);
+          this.router.navigate(['/usuario']);
+        },
+        error => {
+          this.feedback.openSnackBar(error);
+        }
+      );
+    }
+
   }
 
 }
